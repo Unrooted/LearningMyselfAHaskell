cf2c3fb9c613da4934a0ee3a47f04eb2162099ef
Unrooted <konrad.root.klawikowski@gmail.com>
2019-03-11 21:44:19 +0100

    deleting .stack-work folders

--- Examples/DoExample/src/DoExample.hs
+++ Examples/DoExample/src/DoExample.hs
@@ -1,12 +1,11 @@
 f = do
-    a
-    b
-    c
-g = do
-    a
-    b
-    c
-a :: IO ()
+            a
+            b
+            c
 
+g = do a
+            b
+            c
+a :: IO ()
 a = undefined; b = undefined; c = undefined
 main = undefined
